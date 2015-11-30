194c1f25a65644049a63089531e4c57094d1b00d
wrh2 <wrh2@pdx.edu>
2015-11-30 13:50:05 -0800

    Modified c3 code to toggle LEDs
    Changed hex pattern for lighting LEDs on PORTB.

--- tools/c3.c
+++ tools/c3.c
@@ -3,12 +3,10 @@
 int main(void) {
 	int i;
 	while(1) {
-		/*
-		PORTA.GPCLR = 0xFFFFFFFF;
+		PORTB.GPCLR = 0xFFFFFFFF;
 		for(i = 0; i < 1000000; ++i);
-		PORTA.GPCLR = 0xFFFFFFFF;
+		PORTB.GPCLR = 0xFFFF0000;
 		for(i = 0; i < 1000000; ++i);
-		*/
 	}
 	return 0;
 }
