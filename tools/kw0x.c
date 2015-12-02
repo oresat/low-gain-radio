e8dd21c7e48293ef023bc36c036ee38022ad61a6
TheoH <Theo0x48@gmail.com>
2015-12-02 01:06:57 -0800

    Added GPIO, OSC, and SIM register blocks

--- tools/kw0x.c
+++ tools/kw0x.c
@@ -5,3 +5,13 @@ volatile struct PORT * const _PORTB = (struct PORT *) 0x4004A000;
 volatile struct PORT * const _PORTC = (struct PORT *) 0x4004B000;
 volatile struct PORT * const _PORTD = (struct PORT *) 0x4004C000;
 volatile struct PORT * const _PORTE = (struct PORT *) 0x4004D000;
+
+volatile struct GPIO * const _GPIOA = (struct GPIO *) 0x400FF000;
+volatile struct GPIO * const _GPIOB = (struct GPIO *) 0x400FF040;
+volatile struct GPIO * const _GPIOC = (struct GPIO *) 0x400FF080;
+volatile struct GPIO * const _GPIOD = (struct GPIO *) 0x400FF0C0;
+volatile struct GPIO * const _GPIOE = (struct GPIO *) 0x400FF100;
+
+volatile struct OSC * const _OSC0 = (struct OSC *) 0x40065000;
+
+volatile struct SIM_t * const _SIM = (struct SIM_t *) 0x40047000;
--- tools/kw0x.h
+++ tools/kw0x.h
@@ -42,7 +42,6 @@ struct PORT {
 	uint32_t ISFR;  // W1C?
 };
 
-
 extern volatile struct PORT * const _PORTA;
 extern volatile struct PORT * const _PORTB;
 extern volatile struct PORT * const _PORTC;
@@ -54,5 +53,64 @@ extern volatile struct PORT * const _PORTE;
 #define PORTD (*_PORTD)
 #define PORTE (*_PORTE)
 
+struct GPIO {
+	uint32_t PDOR;
+	uint32_t PSOR;
+	uint32_t PCOR;
+	uint32_t PTOR;
+	uint32_t PDIR;
+	uint32_t PDDR;
+};
+
+extern volatile struct GPIO * const _GPIOA;
+extern volatile struct GPIO * const _GPIOB;
+extern volatile struct GPIO * const _GPIOC;
+extern volatile struct GPIO * const _GPIOD;
+extern volatile struct GPIO * const _GPIOE;
+#define GPIOA (*_GPIOA)
+#define GPIOB (*_GPIOB)
+#define GPIOC (*_GPIOC)
+#define GPIOD (*_GPIOD)
+#define GPIOE (*_GPIOE)
+
+struct OSC {
+	uint8_t CR;
+};
+
+extern volatile struct OSC * const _OSC0;
+#define OSC0 (*_OSC0);
+
+
+struct SIM_t {
+	uint32_t SOPT1;
+	uint32_t _PAD0[1024];
+	uint32_t SOPT2;
+	uint32_t _PAD1[1];
+	uint32_t SOPT4;
+	uint32_t SOPT5;
+	uint32_t _PAD2[1];
+	uint32_t SOPT7;
+	uint32_t _PAD3[2];
+	uint32_t SDID;
+	uint32_t _PAD4[3];
+	uint32_t SCGC4;
+	uint32_t SCGC5;
+	uint32_t SCGC6;
+	uint32_t SCGC7;
+	uint32_t CLKDIV1;
+	uint32_t _PAD5[1];
+	uint32_t FCFG1;
+	uint32_t FCFG2;
+	uint32_t _PAD6[1];
+	uint32_t UIDMH;
+	uint32_t UIDML;
+	uint32_t UIDL;
+	uint32_t _PAD7[39];
+	uint32_t COPC;
+	uint32_t SRVCOP;
+};
+
+extern volatile struct SIM_t * const _SIM;
+#define SIM (*_SIM)
 
 #endif
