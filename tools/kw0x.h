cc42530b74568fbb0398e35774896a3530d14ec2
TheoH <Theo0x48@gmail.com>
2015-12-03 19:28:43 -0800

    Improved how register struct packing is handled
    It now warns whenever a padding byte is accessed, and also obfuscates
    the names a bit making it harder to do so on accident.
    
    Also added the packed attribute to all the register structs as a matter
    of best practice - they never should have any alignment padding.

--- tools/kw0x.h
+++ tools/kw0x.h
@@ -3,13 +3,21 @@
 #define _KW0X_H_
 #include <stdint.h>
 
+#define  PASTE(a, b) _PASTE(a, b)
+#define _PASTE(a, b) a ## b
+
+#define PAD_BYTES(bytes)  const uint8_t PASTE(__PAD, __COUNTER__)[bytes] \
+  __attribute__((deprecated("Padding must never be accessed")))
+
+#define PACKED __attribute__((packed))
+
 struct PORT {
 	uint32_t PCR[32];
 	uint32_t GPCLR; // Write only
 	uint32_t GPCHR; // Write only
-	const uint32_t _PAD[7];
+	PAD_BYTES(24);
 	uint32_t ISFR;  // W1C?
-};
+} PACKED;
 
 #define PORTA (*((volatile struct PORT *) 0x40049000))
 #define PORTB (*((volatile struct PORT *) 0x4004A000))
@@ -24,7 +32,7 @@ struct GPIO {
 	uint32_t PTOR;
 	uint32_t PDIR;
 	uint32_t PDDR;
-};
+} PACKED;
 
 #define GPIOA (*((volatile struct GPIO *) 0x400FF000))
 #define GPIOB (*((volatile struct GPIO *) 0x400FF040))
@@ -34,38 +42,38 @@ struct GPIO {
 
 struct OSC {
 	uint8_t CR;
-};
+} PACKED;
 
 #define OSC0 (*((volatile struct OSC *) 0x40065000))
 
 struct SIM_t {
 	uint32_t SOPT1;
-	uint32_t _PAD0[1024];
+	PAD_BYTES(4096);
 	uint32_t SOPT2;
-	uint32_t _PAD1[1];
+	PAD_BYTES(4);
 	uint32_t SOPT4;
 	uint32_t SOPT5;
-	uint32_t _PAD2[1];
+	PAD_BYTES(4);
 	uint32_t SOPT7;
-	uint32_t _PAD3[2];
+	PAD_BYTES(8);
 	uint32_t SDID;
-	uint32_t _PAD4[3];
+	PAD_BYTES(12);
 	uint32_t SCGC4;
 	uint32_t SCGC5;
 	uint32_t SCGC6;
 	uint32_t SCGC7;
 	uint32_t CLKDIV1;
-	uint32_t _PAD5[1];
+	PAD_BYTES(4);
 	uint32_t FCFG1;
 	uint32_t FCFG2;
-	uint32_t _PAD6[1];
+	PAD_BYTES(4);
 	uint32_t UIDMH;
 	uint32_t UIDML;
 	uint32_t UIDL;
-	uint32_t _PAD7[39];
+	PAD_BYTES(156);
 	uint32_t COPC;
 	uint32_t SRVCOP;
-};
+} PACKED;
 
 #define SIM (*((volatile struct SIM_t *) 0x40047000))
 
@@ -78,17 +86,21 @@ struct MCG_t {
 	uint8_t C5;
 	uint8_t C6;
 	uint8_t S;
-	uint8_t _PAD0[1];
+	PAD_BYTES(1);
 	uint8_t SC;
-	uint8_t _PAD1[1];
+	PAD_BYTES(1);
 	uint8_t ATCVH;
 	uint8_t ATCVL;
 	uint8_t C7;
 	uint8_t C8;
-	uint8_t PAD3[1];
+	PAD_BYTES(1);
 	uint8_t C10;
-} __attribute__((packed));
+} PACKED;
 
 #define MCG (*((volatile struct MCG_t *) 0x40064000))
 
+#undef PASTE
+#undef _PASTE
+#undef PAD_BYTES
+#undef PACKED
 #endif
