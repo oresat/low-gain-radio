5d10eb5b0d3c451bc71546b8f41104920a475e43
wrh2 <wrh2@pdx.edu>
2015-12-03 16:54:34 -0800

    Code to blink an LED on the C3 board
    Title says it all. Code covers simple clock config and GPIO config then toggles the GPIO output to make LEDs blink.

--- tools/c3.c
+++ tools/c3.c
@@ -2,10 +2,27 @@
 
 int main(void) {
 	int i;
+
+	/* clock configuration */
+	MCG.C5 = 0xF;
+	MCG.C5 = 0x40;
+	MCG.C6 = 0x40; /* selects PLL instead of FLL */
+	MCG.C2 |= 0x14; /* set encoding to high frequency range, select source for the external reference clock */
+	OSC0.CR = 0x80; /* enable external oscillator */
+	SIM.SCGC5 |= 0x3E00; /* Enable clock for all ports */
+
+	/* modify mux to select alt 1 functionality
+	   for PTB1, PTB2, PTB17.
+	   Alt 1 functionality is just GPIO
+	*/
+	PORTB.PCR[1] |= 0x100;
+	PORTB.PCR[2] |= 0x100;
+	PORTB.PCR[17] |= 0x100;
+	GPIOB.PDDR |= 0x20006; /* set as OUTPUT */
+
 	while(1) {
-		PORTB.GPCLR = 0xFFFFFFFF;
-		for(i = 0; i < 1000000; ++i);
-		PORTB.GPCLR = 0xFFFF0000;
+	  /* toggle output with toggle output register */
+		GPIOB.PTOR = 0xFFFFFFFF;
 		for(i = 0; i < 1000000; ++i);
 	}
 	return 0;
