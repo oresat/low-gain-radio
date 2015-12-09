6c544b6bf207e494918bf538e6f3c27fd8cbf26b
wrh2 <wrh2@pdx.edu>
2015-12-08 22:38:55 -0800

    Clock configuration finished
    The MCU now boots up with the internal oscillator like it should then sets up the port and spi clocks (so that we can read/write those registers) then a small initialization procedure on the SPI0 module requests the full 32MHz signal from the transceiver. When the transceiver supplies that, we shut off all the clocks to all the modules and then dial up the PLL to create a 48 MHz clock signal. After that, we turn the clocks back on for the modules we use in this code (which aren't much some are even completey unnecessary) then blink some LEDs because thats what we do in computer engineering is blink LEDs. Now lets get this thing space ready so we can blink LEDs out in space that no one can see. The end.

--- tools/c3.c
+++ tools/c3.c
@@ -5,59 +5,6 @@
  */
 #include "kw0x.h"
 
-void initialize_clock(void){
-	/* simple clock configuration */
-
-	/* set PLL external reference divider (PRDIV0) to 16 */
-	//MCG.C5 = 0xF;
-
-	/* enable MCGPLLCLK if system is in Normal Stop mode */
-	//MCG.C5 |= 0x40;
-
-	/* select PLL instead of FLL */
-	//MCG.C6 = 0x40;
-	//MCG.C6 |= 0x68;
-
-	/* set frequency range select to high frequency range for oscillator
-	   and select external reference clock
-	 */
-	//MCG.C2 |= 0x14;
-	//MCG.C2 |= 0x10;
-
-	/* enable external oscillator */
-	//OSC0.CR = 0x80;
-
-	/* enable clock for all ports */
-	SIM.SCGC5 |= 0x3E00;
-
-	/* enable clock for SPI modules */
-	SIM.SCGC4 |= 0xC00000;
-
-	/* enable clock for TPM, RTC modules */
-	SIM.SCGC6 |= 0x27000000;
-
-	return;
-}
-
-void initialize_gpio(void){
-
-	/* modify mux to select alt 1 functionality
-	   for PTB1, PTB2, PTB17.
-	   Alt 1 functionality is just GPIO
-	*/
-	PORTB.PCR[1] |= 0x100;
-	PORTB.PCR[2] |= 0x100;
-	PORTB.PCR[17] |= 0x100;
-
-	/* set data direction as output */
-	GPIOB.PDDR |= 0x20006;
-
-	/* turn all LEDs of by pulling pins high */
-	GPIOB.PTOR = 0x20006;
-
-	return;
-}
-
 int initialize_spi(void){
 	/* now let's set up the SPI interface for the transceiver */
 	/* first select the proper mux setting on each pin needed */
@@ -77,16 +24,89 @@ int initialize_spi(void){
 	/* write to SPI0.DH and SPI0.DL */
 	SPI0.DH = 0xA6;		/* we want to write to register 0x26 in the transceiver, but it needs be encoded with a 1 at the MSB for a write */ 
 	SPI0.DL = 0x00;		/* clear the bits in register 0x26 of the transceiver block */
-	while(SPI0.S & (1 << 7)); /* wait for read buffer full flag */
-	volatile uint16_t mySPI = (uint16_t) SPI0.DL; /* read the lower half of the data register */
+	//while(SPI0.S & (1 << 7)); /* wait for read buffer full flag */
 
+	/* success! */
 	return 1;
 }
 
+void initialize_clock(void){
+	/* simple clock configuration that involves initializing the SPI so we can get the external clock reference from the transceiver */
+
+	/* enable clock for SPI modules */
+	SIM.SCGC4 |= 0xC00000;
+
+	/* enable clock for all ports */
+	SIM.SCGC5 |= 0x3E00;
+
+	/* note: if this function doesn't return, you're screwed. */
+	if(initialize_spi()){
+
+	  	/* disable SPI module clock because theo said it would get mad if I didn't! */
+	  	SIM.SCGC4 &= 0xFF3FFFFF;
+
+		/* disable port module clock because theo said it would get mad if I didn't! */
+		SIM.SCGC5 &= 0xFFFFC1FF;
+
+		/* set PLL external reference divider (PRDIV0) to 16, this will give us 2 MHz */
+		MCG.C5 = 0xF;
+
+		/* enable MCGPLLCLK if system is in Normal Stop mode */
+		MCG.C5 |= 0x40;
+
+		/* select PLL instead of FLL */
+		MCG.C6 |= 0x40;
+
+		/* set frequency range select to high frequency range for oscillator
+		   and select external reference clock
+	 	*/
+		MCG.C2 |= 0x14;
+
+		/* enable external oscillator */
+		OSC0.CR = 0x80;
+	}
+
+	/* now enable clock for SPI modules, AGAIN! */
+	SIM.SCGC4 |= 0xC00000;
+
+	/* enable clock for all ports */
+	SIM.SCGC5 |= 0x3E00;
+
+	/* enable clock for TPM, RTC modules */
+	SIM.SCGC6 |= 0x27000000;
+
+	return;
+}
+
+void initialize_gpio(void){
+	/* procedure for getting the GPIO going on pins PTB1, PTB2, PTB17 */
+
+	/* modify mux to select alt 1 functionality
+	   for PTB1, PTB2, PTB17.
+	   Alt 1 functionality is just GPIO
+	*/
+	PORTB.PCR[1] |= 0x100;
+	PORTB.PCR[2] |= 0x100;
+	PORTB.PCR[17] |= 0x100;
+
+	/* set data direction as output */
+	GPIOB.PDDR |= 0x20006;
+
+	/* turn all LEDs of by pulling pins high */
+	GPIOB.PTOR = 0x20006;
+
+	return;
+}
+
 void initialize_tpm(void){
+  	/* procedure for initializing Timer/PWM module */
+
+	/* Clock mode select for each module, make TPM counter increment on every TPM counter clock */
   	TPM0.SC = 0x8;
 	TPM1.SC = 0x8;
 	TPM2.SC = 0x8;
+
+	/* more needs to be done here, maybe? */
   	return;
 }
 
@@ -97,18 +117,14 @@ int main(void) {
 	/* call initialization procedures */
 	initialize_clock();
 	initialize_gpio();
-	int myInit = initialize_spi(); /* if this returns, we're good */
 	initialize_tpm();
 
 	while(1) {
-	  	/* if we get here, myInit should be set, and then we light some LEDs */
-	  	if(myInit){
-		  	/* toggle LED connected to PTB2 */
-	  		GPIOB.PTOR = 0x00004;
+		/* toggle LED connected to PTB2 */
+	  	GPIOB.PTOR = 0x00004;
 
-			/* delay loop */
-			for(i = 0; i < 1000000; ++i);
-	  	}
+		/* delay loop */
+		for(i = 0; i < 1000000; ++i);
 	}
 	return 0;
 }
