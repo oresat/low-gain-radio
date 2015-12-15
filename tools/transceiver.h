36eb63cd69315ac6957e82996cc2dd50417e87da
wrh2 <wrh2@pdx.edu>
2015-12-15 12:35:13 -0800

    Transceiver register mapping
    Just saving my progress.

--- tools/transceiver.h
+++ tools/transceiver.h
@@ -37,6 +37,48 @@ struct TRANSCEIVER {
 	uint8_t RegFeiMsb; /* MSB of the calculated frequency error */
 	uint8_t RegFeiLsb; /* LSB of the calculated frequency error */
 	uint8_t RegRssiConfig; /* RSSI-related settings */
+	uint8_t RegRssiValue; /* RSSI value in dBm */
+	uint8_t RegDioMapping1; /* Mapping of pins DIO0 to DIO3 */
+	uint8_t RegDioMapping2; /* Mapping of pins DIO4 and DIO5/Clkout frequency */
+	uint8_t RegIrqFlags1; /* Status register: PLL Lock state, Timeout, RSSI > Threshold... */
+	uint8_t RegIrqFlags2; /* Status register: FIFO handling flags, Low battery detection... */
+	uint8_t RegRssiThresh; /* RSSI Threshold control */
+	uint8_t RegRxTimeout1; /* Timeout duration between RX request and RSSI detection */
+	uint8_t RegRxTimeout2; /* Timeout duration between RSSI detection and PayloadReady */
+	uint8_t PreambleMsb; /* Preamble length, MSB */
+	uint8_t PreambleLsb; /* Preamble length, LSB */
+	uint8_t RegSyncConfig; /* Sync Word Recognition control */
+	uint8_t RegSyncValue1; /* Sync Word byte 1 */
+	uint8_t RegSyncValue2; /* Sync Word byte 2 */
+	uint8_t RegSyncValue3; /* Sync Word byte 3 */
+	uint8_t RegSyncValue4; /* Sync Word byte 4 */
+	uint8_t RegSyncValue5; /* Sync Word byte 5 */
+	uint8_t RegSyncValue6; /* Sync Word byte 6 */
+	uint8_t RegSyncValue7; /* Sync Word byte 7 */
+	uint8_t RegSyncValue8; /* Sync Word byte 8 */
+	uint8_t RegPacketConfig; /* Packet mode settings */
+	uint8_t RegPayloadLength; /* Payload length setting */
+	uint8_t RegNodeAdrs; /* Node address */
+	uint8_t RegBroadcastAdrs; /* Broadcast address */
+	uint8_t RegAutoModes; /* Auto modes settings */
+	uint8_t RegFifoThresh; /* Fifo threshold, TX start condition */
+	uint8_t RegPacketConfig2; /* Packet mode settings */
+	uint8_t RegAesKey1; /* cypher key, byte 1 */
+	uint8_t RegAesKey2; /* cypher key, byte 2 */
+	uint8_t RegAesKey3; /* cypher key, byte 3 */
+	uint8_t RegAesKey4; /* cypher key, byte 4 */
+	uint8_t RegAesKey5; /* cypher key, byte 5 */
+	uint8_t RegAesKey6; /* cypher key, byte 6 */
+	uint8_t RegAesKey7; /* cypher key, byte 7 */
+	uint8_t RegAesKey8; /* cypher key, byte 8 */
+	uint8_t RegAesKey9; /* cypher key, byte 9 */
+	uint8_t RegAesKey10; /* cypher key, byte 10 */
+	uint8_t RegAesKey11; /* cypher key, byte 11 */
+	uint8_t RegAesKey12; /* cypher key, byte 12 */
+	uint8_t RegAesKey13; /* cypher key, byte 13 */
+	uint8_t RegAesKey14; /* cypher key, byte 14 */
+	uint8_t RegAesKey15; /* cypher key, byte 15 */
+	uint8_t RegAesKey16; /* cypher key, byte 16 */
 };
 
 
