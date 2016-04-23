681f0e4570f031b7383544fe962e3b836c838080
Theo Hill <Theo0x48@gmail.com>
2016-04-22 23:41:35 -0700

    Fixes and cleanups in UART driver
    - Turned some magic numbers into descriptive macros
    - Fixed UART0 clock source from internal reference to PLL
    - Reduced code duplication between uart_ and uart0_ functions
    - Fixed uart_init enabling both UART[1,2] clocks for only one driver
    - Fixed uart_read writing to UART data register instead of reading

--- tools/drivers/uart.c
+++ tools/drivers/uart.c
@@ -29,92 +29,67 @@ static const struct pin_assign RX [] = {
 	{}
 };
 
+//SIM.SCGC4
+#define SCGC_UART0 (1 << 10)
+#define SCGC_UART1 (1 << 11)
+#define SCGC_UART2 (1 << 12)
+//SIM.SOPT2
+#define UART0SRC_MCGIRCLK (3 << 26)
+#define UART0SRC_MCGPLLFLLCLK (1 << 26)
+
+#define PLLFLLSEL (1 << 16)
+
+//
+#define TDRE (1 << 7)
+#define RDRF (1 << 5)
+//C2
+#define TE (1 << 3)
+#define RE (1 << 2)
+
 void uart0_init(volatile struct uart0 * UART, const struct uart_config * config){
 	/* select UART0 clock source to PLL */
 	/* note: this step isn't needed for UART1/UART2
            since they are run off the bus clock */
-	SIM.SOPT2 |= 0xC010000;
+	SIM.SOPT2 |= UART0SRC_MCGPLLFLLCLK | PLLFLLSEL;
 
-	/* enable clock for UART0 */
-	SIM.SCGC4 |= 0x400;
-
-	/* setup pins */
-	set_pin_alt(TX, UART, &config->TX);
-	set_pin_alt(RX, UART, &config->RX);
-	
-	//UART->BDH = 0x1d;
-	//UART->BDL = 0xb7;
-
-	/* br = baud clock / concat(BDH[4:0], BDL[7:0]) */
-	/* baud clock for UART1/UART2 is the bus clock
-           which is the system clock divided by 2
-	   see UART section of data book for equation
-	   and section 4 on clock distribution for clock definitions
-
-	   the calculation below assumes baud clock = 24MHz
-	*/
-	UART->BDH = 0x0;
-	UART->BDL = 0xD0;
-
-	UART->C1 = 0x0;
-
-	/* something is trigger a transaction right after this line and it never completes */
-	UART->C2 = 0xC;
-
-	UART->C3 = 0x0;
-	UART->C4 = 0x0;
+	/* enable clock*/
+	SIM.SCGC4 |= SCGC_UART0;
+	uart_init((volatile struct uart *) UART, config);
 }
 
 void uart0_read(volatile struct uart0 * UART, size_t len, uint8_t * buffer){
-	if(!len) return;
-
-	for(unsigned int i = 0; i < len; ++i){
-		/* poll receive data register full flag */
-		while(!(UART->S1 & (1 << 5)));
-
-		UART->D = buffer[i];
-	}
+	uart_read((volatile struct uart *) UART, len, buffer);
 }
 
 void uart0_write(volatile struct uart0 * UART, size_t len, uint8_t * buffer){
-	if(!len) return;
-	
-	/* write bytes to data register */
-	for(unsigned int i = 0; i < len; ++i){
-		UART->D = buffer[i];
-
-		/* poll transmit data register empty flag */
-		while(!(UART->S1 & (1 << 7)));
-	}
+	uart_write((volatile struct uart *) UART, len, buffer);
 }
 
 
 void uart_init(volatile struct uart * UART, const struct uart_config * config){
-	/* enable clock for UART1, UART2 */
-	SIM.SCGC4 |= 0x1800;
+	/* enable clock */
+	if(UART == &UART1)
+		SIM.SCGC4 |= SCGC_UART1;
+	if(UART == &UART2)
+		SIM.SCGC4 |= SCGC_UART2;
 
 	/* setup pins */
 	set_pin_alt(TX, UART, &config->TX);
 	set_pin_alt(RX, UART, &config->RX);
 
-	//UART->BDH = 0x1d;
-	//UART->BDL = 0xb7;
-
 	/* br = baud clock / concat(BDH[4:0], BDL[7:0]) */
-	/* baud clock for UART1/UART2 is the bus clock
-           which is the system clock divided by 2
-	   see UART section of data book for equation
-	   and section 4 on clock distribution for clock definitions
+	/* baud clock for UART[1,2] is the bus clock which is the system clock
+	   divided by 2. See UART section of data book for equation and section
+	   4 on clock distribution for clock definitions
 
 	   the calculation below assumes baud clock = 24MHz
 	*/
 	UART->BDH = 0x0;
 	UART->BDL = 0xD0;
+	//UART->BDH = 0x1d;
+	//UART->BDL = 0xb7;
 
-	UART->C1 = 0x0;
-	UART->C2 = 0xC;
-	UART->C3 = 0x0;
-	UART->C4 = 0x0;
+	UART->C2 = TE | RE;
 }
 
 void uart_read(volatile struct uart * UART, size_t len, uint8_t * buffer){
@@ -122,9 +97,9 @@ void uart_read(volatile struct uart * UART, size_t len, uint8_t * buffer){
 
 	for(unsigned int i = 0; i < len; ++i){
 		/* poll receive data register full flag */
-		while(!(UART->S1 & (1 << 5)));
+		while(!(UART->S1 & RDRF));
 
-		UART->D = buffer[i];
+		buffer[i] = UART->D;
 	}
 }
 
@@ -136,6 +111,6 @@ void uart_write(volatile struct uart * UART, size_t len, uint8_t * buffer){
 		UART->D = buffer[i];
 
 		/* poll transmit data register empty flag */
-		while(!(UART->S1 & (1 << 7)));
+		while(!(UART->S1 & TDRE));
 	}
 }
