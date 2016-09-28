4fa276c5d393003e326078b6d3ded1f3e2d281b4
Eddie <estrandb@gmail.com>
2016-09-27 20:33:14 -0700

    tpm updates from v12

--- firmware/drivers/tpm.c
+++ firmware/drivers/tpm.c
@@ -1,3 +1,10 @@
+/*! \file tpm.c
+ * \brief   TPM Peripheral
+ *
+ * \defgroup tpm_peripheral
+ * @{
+ */
+
 #include <stdbool.h>
 
 #include "v1_1.h"
@@ -12,20 +19,28 @@
 #include "em-printf.h"
 
 #include "tpm.h"
-#define TPM0_MOD_1uS_at_24Mhz       23 * 100
-#define TPM0_MOD_1mS_at_24Mhz       23999
 
-static bool debug_isr_count = 0;
-static bool tpm0_count_flag = false;
+#define TPM0_MOD_50uS_at_24Mhz       1199
+#define TPM0_MOD_100uS_at_24Mhz      2399
+#define TPM0_MOD_250uS_at_24Mhz      5999
+#define TPM0_MOD_500uS_at_24Mhz      11999
+#define TPM0_MOD_1mS_at_24Mhz        23999
+
+static bool     debug_isr_count  = 0;
+static bool     tpm0_count_flag  = false;
+
+static uint32_t TPM0_MOD_PERIOD  = TPM0_MOD_1mS_at_24Mhz;
+
+static void reset_tof(void)
+{
+	TPM0.C0SC |= TPM_C0SC_CHF;
+	TPM0.STATUS |= TPM_STATUS_TOF;
+	TPM0.SC |= TPM_SC_TOF;
+}
+
 
 void isr_tpm0(void)
 {
-	debug_isr_count++;
-	if(debug_isr_count % 1000000)
-	{
-		led_action(TOGGLE, blue);
-	}
-
 	//read TOF register, if bit in tfo is set, then proceed.
 	if ((TPM0.SC & TPM_SC_TOF) != 0)
 	{
@@ -38,84 +53,135 @@ void isr_tpm0(void)
 	}
 }
 
-void tpm0_mod_1us(void)
+/*! High accuracy interrupts from timer0 ISR
+ */
+static void tpm0_mod_50us(void)
 {
+	TPM0_MOD_PERIOD = TPM0_MOD_50uS_at_24Mhz;
 	// word or halfword access only
-	__STR(TPM0_MOD_1uS_at_24Mhz, &TPM0.MOD);
+	__STR(TPM0_MOD_50uS_at_24Mhz, &TPM0.MOD);
 }
 
-void tpm0_mod_1ms(void)
+static void tpm0_mod_1ms(void)
 {
+	TPM0_MOD_PERIOD = TPM0_MOD_1mS_at_24Mhz;
 	// word or halfword access only
 	__STR(TPM0_MOD_1mS_at_24Mhz, &TPM0.MOD);
 }
-
-void reset_tof()
-{
-	TPM0.C0SC |= TPM_C0SC_CHF;
-	TPM0.STATUS |= TPM_STATUS_TOF;
-	TPM0.SC |= TPM_SC_TOF;
-}
-
-void disable_tpm()
+static void disable_tpm(void)
 {
 	TPM0.SC &= TPM_SC_DISABLE;
 }
 
-void tpm0_enable_int()
+static void tpm0_enable_int(void)
 {
 	TPM0.SC |= TPM0_TOIE;
 }
 
-void tpm0_disable_int(void)
+static void tpm0_disable_int(void)
 {
 	TPM0.SC &= TPM0_TOIE_MASK;
 }
 
-void tpm0_sc_init()
+static void tpm0_sc_init(void)
 {
 	TPM0.SC   = TPM0_SC;
 }
-void tpm0_conf_init()
+
+static void tpm0_conf_init(void)
 {
 	TPM0.CONF = TPM0_CONF_LGR;
 	TPM0.C0SC = (0b01 << 4); // software compare
 }
 
-void tpm0_init()
+/*!
+ *  \warning Must run: tpm0_enable_int() to use tpm0 isr
+ */
+void tpm0_init(bool highres)
 {
-	//led_action(TOGGLE, green);
+	__disable_irq();
 	NVIC_EnableIRQ(TPM0_IRQn);
-	__enable_irq();
-	enable_tpm_clocks();
+
+	enable_tpm_mcg_clock();
 	tpm0_conf_init();
-	led_action(ON, red);
-	tpm0_mod_1ms();
-	led_action(OFF, red);
+	if(highres)
+	{
+		tpm0_mod_50us();
+	}
+	else
+	{
+		tpm0_mod_1ms();
+	}
 	tpm0_sc_init();
-	tpm0_enable_int();
 	tpm0_count_flag  = false;
+
+	__enable_irq();
 }
 
-void    tpm0_test_loop()
+/*!
+ * Enable the ISR and wait for integer n number of milliseconds
+ *
+ */
+void wait_n_ms(uint32_t n)
 {
-	uint32_t tpm0_count = 0;
-	led_action(OFF, blue);
-	led_action(OFF, green);
-	led_action(OFF, red);
-	while(1)
+	if(TPM0_MOD_PERIOD == TPM0_MOD_1mS_at_24Mhz)
 	{
-		if(tpm0_count_flag)
+		// write the count register to clear it
+		TPM0.CNT = 0;
+		tpm0_enable_int();
+		for(uint32_t tpm0_count = 0; tpm0_count < n; )
 		{
-			led_action(TOGGLE, blue);
-			tpm0_count++;
-			tpm0_count_flag = false;
-			if ((tpm0_count != 0) && ((tpm0_count % 1000) == 0))
+			if(tpm0_count_flag)
 			{
-				led_action(TOGGLE, red);
-				printf("tic\t%d\r\n", tpm0_count);
+				led_action(TOGGLE, led7);
+				tpm0_count++;
+				tpm0_count_flag = false;
 			}
 		}
 	}
+	else if (TPM0_MOD_PERIOD == TPM0_MOD_50uS_at_24Mhz)
+	{
+		// write the count register to clear it
+		TPM0.CNT = 0;
+		tpm0_enable_int();
+		for(uint32_t tpm0_count = 0; tpm0_count < n * 20; )
+		{
+			if(tpm0_count_flag)
+			{
+				led_action(TOGGLE, led7);
+				tpm0_count++;
+				tpm0_count_flag = false;
+			}
+		}
+
+	}
+	else
+	{
+		#ifdef TPM_TEST
+		printf("TPM0_MOD_PERIOD undefined");
+		#endif
+		while(1)
+		{
+			;
+		}
+	}
+	tpm0_disable_int();
 }
 
+#ifdef TPM_TEST
+void tpm0_test_loop()
+{
+	led_action(OFF, led5);
+	led_action(OFF, led6);
+	led_action(OFF, led7);
+	led_action(OFF, led8);
+	while(1)
+	{
+		wait_n_ms(500);
+		led_action(TOGGLE, led8);
+		printf("*");
+	}
+}
+#endif
+
+/*! @} */
